5b543f87d3ea69e910f0d6494fb3a39de49586f7
Michael Ken Revil <60070288+mkhrvl@users.noreply.github.com>
2024-06-27 16:44:18 +0800

    feat: add button to create new grid

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const btnGrid = document.createElement('button');
+btnGrid.classList.add('btn-grid');
+btnGrid.textContent = 'Create A New Grid';
+document.body.appendChild(btnGrid);
+
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
@@ -13,5 +18,3 @@ for (let i = 0; i < 16; i++) {
         rowContainer.appendChild(rowItem);
     }
 }
-
-
