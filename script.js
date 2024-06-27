b6bcd6cfa7930f68ad2877e19defad07e6bf1ecb
Michael Ken Revil <60070288+mkhrvl@users.noreply.github.com>
2024-06-27 16:18:19 +0800

    feat: create initial grid programmatically

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+    const rowContainer = document.createElement('div');
+    rowContainer.classList.add('row');
+    container.appendChild(rowContainer);
+
+    for (let j = 0; j < 16; j++) {
+        const rowItem = document.createElement('div');
+        rowItem.classList.add('row__item');
+        rowContainer.appendChild(rowItem);
+    }
+}
+
+
